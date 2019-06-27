b38ff7fd3344405f33623915aaef377b0a13de0f
Simon LEDUNOIS <simon.ledunois@gmail.com>
2019-06-27 14:38:10 +0200

    Init project

---
+++ src/main/java/fr/openent/mediacentre/Mediacentre.java
@@ -0,0 +1,12 @@
+package fr.openent.mediacentre;
+
+import org.entcore.common.http.BaseServer;
+
+public class Mediacentre extends BaseServer {
+
+	@Override
+	public void start() throws Exception {
+		super.start();
+	}
+
+}
